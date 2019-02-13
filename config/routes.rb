479ffb88776f206ac3daeab4f6edc327677c8af6
nagit555 <47467964+nagit555@users.noreply.github.com>
2019-02-13 18:22:04 +0900

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # :index,:create,:new,:edit,:show,:update,:destroy
+  resources :users    #, only: []
+  resources :groups
+  resources :members
+  resources :messages
+
+  root 'messages#index'
 end
